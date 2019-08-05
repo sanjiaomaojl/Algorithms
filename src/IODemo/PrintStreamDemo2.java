b8687fd3e4a2f3e5eea13b356e6a49dbd3d77e90
sanjiaomaojl <1183781747@qq.com>
2019-08-05 18:54:01 +0800

    PS Text,because modify email

--- src/IODemo/PrintStreamDemo2.java
+++ src/IODemo/PrintStreamDemo2.java
@@ -5,10 +5,10 @@ import java.io.PrintStream;
 
 public class PrintStreamDemo2 {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("sfskjk");
+        System.out.println("也是测试哦");
         PrintStream ps = new PrintStream("kmk.txt");
         //change something
         System.setOut(ps);
-        System.out.println("mfknfh");
+        System.out.println("测试一下");
     }
 }
